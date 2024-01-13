c54ebe28782d869d81f537e69f44867472c6b0e2
May Ely <145827586+Ylyaem@users.noreply.github.com>
2024-01-13 13:25:56 -0600

    Exercise 2
    Learning Activity - C# Prep 2 made

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("Enter your score: ");
+        string score = Console.ReadLine();
+
+        int score_number = int.Parse(score);
+
+        string letter_grade = "";
+
+
+        if (score_number >= 90)
+
+         {letter_grade = "A";}
+
+        else if (score_number >= 80)
+         {letter_grade = "B";}
+
+        else if (score_number >= 70)
+         {letter_grade = "C";}
+
+        else if (score_number >= 60)
+         {letter_grade = "D";}
+        
+        else 
+
+         {letter_grade = "C";}
+        
+        Console.WriteLine($"Your grade is {letter_grade}");
+
+        if (score_number >= 70) 
+         {Console.WriteLine("You passed");}
+        else
+         {Console.WriteLine("Try again in the next term");}
+
+         
+
     }
 }
+

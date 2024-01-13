a6a5ac62d55ba90659d82d5809d03ab927c26fb0
May Ely <145827586+Ylyaem@users.noreply.github.com>
2024-01-13 14:55:41 -0600

    Exercise 3
    Complete magic number guest.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
-    }
-}
+
+        Console.Write("What is the magic number? ");
+        int number_magic = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int number_guest = -1;
+
+        while (magicNumber != number_guest )
+               
+        {Console.Write("What is your guest? ");
+        number_guest = int.Parse(Console.ReadLine());
+        if (magicNumber > number_guest)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < number_guest)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You get it!");
+            }
+        }
+
+
+    
+
+        
+
+}}

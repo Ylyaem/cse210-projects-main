bd86ba6464c7b127e447d996906570fe8bca0d32
May Ely <145827586+Ylyaem@users.noreply.github.com>
2024-01-13 12:23:47 -0600

    C# Programming - exercise 1
    C# Prep 1—Variables, Input, and Output activity made.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,20 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+
+        Console.Write("Wnat's your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What's your last name? ") ;
+        string last_name = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_name},  {first_name} {last_name}.");
+
+
+
+
     }
 }
+
+
+

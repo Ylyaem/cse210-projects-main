57cb590fde7e707caa15c268d13e2c4a970b2f68
May Ely <145827586+Ylyaem@users.noreply.github.com>
2024-01-13 20:25:44 -0600

    Excercise 5
    Functions Excercises Made

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,69 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
+
+
     {
         Console.WriteLine("Hello Prep5 World!");
+
+        DisplayWelcomeMessage();
+
+        string user_name = PromptUserName();
+        int user_number = PromptUserNumber();
+
+        int square_number= SquareNumber(user_number);
+
+        DisplayResult(user_name,square_number);
+
+
+
+
+        static void DisplayWelcomeMessage()
+        {Console.WriteLine("Welcome to the program!");}
+
+        static string PromptUserName()
+        { Console.Write("Please enter your name: ");
+
+          string name = Console.ReadLine() ;
+
+          return name ;
+          }
+
+        static int PromptUserNumber()
+
+         { Console.Write("Please enter your favorite number: ");
+
+          int number = int.Parse(Console.ReadLine() ) ;
+
+          return number ;
+          }
+
+        static int SquareNumber(int user_number)
+
+        {int square_number = user_number * user_number ;
+        
+        return square_number ; 
+        
+        }
+
+        static void DisplayResult(string user_name, int square_number)
+
+
+         {Console.WriteLine($"Brother {user_name}, the square of your number is {square_number}");}
+
+
+    
+
+        
+
+
+
+
+
+        
+
     }
 }
